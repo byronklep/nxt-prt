f705c80f5ff9ab16253ca76d611563e996f9460d
Byron Klepper <byronklep@gmail.com>
2021-07-04 19:38:14 -0500

    Initial commit

---
+++ pages/projects.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function projects() {
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5">Projects</h1>
+    </Layout>
+  )
+}
