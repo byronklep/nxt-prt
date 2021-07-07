f37991dd4172ce5784e5a9acc82563d82cdd6586
Byron Klepper <byronklep@gmail.com>
2021-07-07 18:33:28 -0500

    Dark mode implemented.

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
+import { useTheme } from 'next-themes'
 import Link from 'next/link'
+import { BeakerIcon } from '@heroicons/react/solid'
 
 export default function Header() {
+  const { theme, setTheme } = useTheme()
   return (
     <div className="w-full py-10 px-4 sm:px-6 lg:px-0">
       <div className="flex flex-col sm:flex-row justify-between max-w-3xl mx-auto items-center">
@@ -37,6 +40,15 @@ export default function Header() {
             <Link href="/contact">
               <a className="text-gray-900 hover:text-gray-700">Contact</a>
             </Link>
+          </li>
+          <li className="ml-4">
+            <button
+              aria-label="Toggle Dark Mode"
+              type="button"
+              className="  md:order-3"
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+              <BeakerIcon className="h-5 w-5 text-indigo-500" />
+            </button>
           </li>
         </ul>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,15 @@ import '../styles/globals.css'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </ThemeProvider>
   )
 }
 
