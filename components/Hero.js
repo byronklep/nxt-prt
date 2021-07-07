729ee0f10141631adc01c5d1f7c552cb0140edc1
Byron Klepper <byronklep@gmail.com>
2021-07-07 09:16:55 -0500

    Hero & styling on Home page.

---
+++ components/Hero.js
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <div className="max-w3xl mx-auto px-4 sm:px-6 lg:px-0 flex flex-col lg:items-center py-20">
+      <h1 className="text-6xl text-gray-900 text-center font-bold max-w-lg">
+        Welcome to <span className="text-green-700">Sam&apos;s Portfolio</span>
+      </h1>
+      <p className="text-gray-700 font-semibold text-xl mt-8 lg:text-center">
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim
+        lacinia nunc. Curabitur tortor.
+      </p>
+    </div>
+  )
+}
